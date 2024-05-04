c383c6cae1cb334f91dacaa200856201d818f7f5
Ihor Bocharov <ibocharov5@gmail.com>
2024-05-04 17:53:03 +0300

    Evaluation

--- AI-Search/__init__.py
+++ AI-Search/__init__.py
@@ -16,15 +16,16 @@ def main():
 
     #llama_index.core.set_global_handler("simple")
 
-    Settings.llm = OpenAI(temperature=0.0, model="gpt-4")
+    #Settings.llm = OpenAI(temperature=0.0, model="gpt-4")
+    Settings.llm = OpenAI(temperature=0.0, model="gpt-3.5-turbo")
     Settings.chunk_size = 512
     Settings.context_window = 4096
     Settings.num_output = 256
 
     questions = ["Who is John McCarthy?", "Who is Sam Altman?"]
 
-    run_basic_vector_pipeline(questions, True)
-    run_graph_pipeline(questions, True)
+    run_basic_vector_pipeline(questions, False)
+    #run_graph_pipeline(questions, True)
 
 if __name__ == "__main__":
     main()
--- AI-Search/basic_vector/pipeline.py
+++ AI-Search/basic_vector/pipeline.py
@@ -1,5 +1,7 @@
 from llama_index.core import SimpleDirectoryReader, StorageContext, VectorStoreIndex, load_index_from_storage, Settings
 from llama_index.core.evaluation import FaithfulnessEvaluator, RelevancyEvaluator
+from llama_index_client import EvalQuestionResult
+from llama_index.core.schema import NodeWithScore
 
 def run_pipeline(questions: list[str], load_from_storage: bool):
     # Settings
@@ -27,21 +29,51 @@ def run_pipeline(questions: list[str], load_from_storage: bool):
 
     #Query pipeline
     query_engine = index.as_query_engine()
+    retriever = index.as_retriever(similarity_top_k=2)
 
-    print("Basic vector started", end='\n')
+    print("==================================================", end='\n')
+    print("Basic vector started", end="\n\n")
           
     for question in questions:
+        print("--------------------------------------------------", end="\n\n")
+
         response = query_engine.query(question)
         print("Q : " + question, end='\n')
-        print("A : " + str(response), end='\n')
+        print("A : " + str(response), end="\n\n")
+  
+        print("Retrieved nodes ->", end="\n")
+        retrieved_nodes = retriever.retrieve(question)
+        for node in retrieved_nodes:
+            print()
+            display_node(node)
 
+        print()
+        print("*** Response Evaluation ***", end='\n')
         faithfulness_eval_result = faithfulness_evaluator.evaluate_response(response=response)
-        print("Evaluating Response Faithfulness : " + str(faithfulness_eval_result.passing), end='\n')
-        
+        display_evaluation_result(faithfulness_eval_result)
+        print()
+
         relevancy_eval_result = relevancy_evaluator.evaluate_response(query=question, response=response)
-        print("Evaluating Response Relevancy : " + str(relevancy_eval_result), end='\n')
+        display_evaluation_result(relevancy_eval_result)
 
-        print("----------", end='\n')
-
+    print()
     print("Basic vector finished", end='\n')
-    print("==========", end='\n')
+    print("==================================================", end="\n\n")
+
+def display_evaluation_result(result: EvalQuestionResult):
+    print("Evaluating Response Relevancy -> ", end='\n')
+    #print("Query : " + str(relevancy_eval_result.query), end='\n')
+    #print("Contexts : " + str(relevancy_eval_result.contexts), end='\n')
+    #print("Response : " + str(relevancy_eval_result.response), end='\n')
+    print("Passing : " + str(result.passing), end='\n')
+    print("Feedback : " + str(result.feedback), end='\n')
+    print("Score : " + str(result.score), end='\n')
+    print("Pairwise source : " + str(result.pairwise_source), end='\n')
+    print("Invalid result : " + str(result.invalid_result), end='\n')
+    print("Invalid reason : " + str(result.invalid_reason), end='\n')
+
+def display_node(node: NodeWithScore):
+    print("Node ID  : " + str(node.node_id ), end='\n')
+    print("Score    : " + str(node.score ), end='\n')
+    print("Text     : " + str(node.text ), end='\n')
+    #print("Metadata : " + str(node.metadata ), end='\n')
